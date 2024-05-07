d19815222868026272b8577f3e48ca7972392c03
Benjamin Vincent Schulenburg <benjamin.schulenburg@institut-fuer-digitales-lernen.de>
2024-05-07 16:23:04 +0200

    Some fancyness

---
+++ src/assets/header/index.ts
@@ -0,0 +1,31 @@
+import header1 from "./1.jpg";
+import header2 from "./2.jpg";
+import header3 from "./3.jpg";
+import header4 from "./4.jpg";
+import header5 from "./5.jpg";
+import header6 from "./6.jpg";
+import header7 from "./7.jpg";
+import header8 from "./8.jpg";
+import header9 from "./9.jpg";
+import header10 from "./10.jpg";
+import header11 from "./11.jpg";
+import header12 from "./12.jpg";
+import header13 from "./13.jpg";
+import header14 from "./14.jpg";
+
+export const headerImages = [
+	header1,
+	header2,
+	header3,
+	header4,
+	header5,
+	header6,
+	header7,
+	header8,
+	header9,
+	header10,
+	header11,
+	header12,
+	header13,
+	header14
+];
---
+++ src/components/ImageShow.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import css from './ImageShow.module.css';
+import type { ImageMetadata } from 'astro';
+
+interface ImageShowProps {
+	images: ImageMetadata[];
+}
+
+export const ImageShow: React.FC<ImageShowProps> = ({images}) => {
+	const [index, setIndex] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setIndex((prevCounter) => (prevCounter + 1) % images.length);
+		}, 3000);
+	
+		return () => clearInterval(interval);
+	}, []);
+
+	return (<div className={css.show}>
+		{images.map((image, i) => (<img key={i} className={i === index  ? `${css.img} ${css.active}` : css.img} src={image.src} width={image.width} height={image.height} />))}
+	</div>);
+};
