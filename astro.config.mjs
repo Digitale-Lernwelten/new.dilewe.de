18c1b6df8c8543d5fc1a8308677296452c4bb384
Felix Bornheim <felix.bornheim@dilewe.de>
2026-02-27 08:50:27 -0500

    KI Auffindbarkeit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), relativeLinks()]
+  site: "https://dilewe.de",
+  integrations: [react(), sitemap(), relativeLinks()]
 });
