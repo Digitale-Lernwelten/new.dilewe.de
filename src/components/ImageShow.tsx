f86004c4a24397d81d9da1473dedd84317abefd1
Felix Bornheim <felix.bornheim@dilewe.de>
2026-06-08 10:57:27 +0200

    Update Forschung, Geschichte, Referenzen und Publikationen

--- src/components/ImageShow.tsx
+++ src/components/ImageShow.tsx
@@ -8,21 +8,29 @@ interface ImageShowProps {
 }
 
 export const ImageShow: React.FC<ImageShowProps> = ({images, imageAlt = "Impression der Digitalen Lernwelten"}) => {
-	const [index, setIndex] = useState(0);
+	const [order, setOrder] = useState<number[]>(() => images.map((_, i) => i));
+	const [step, setStep] = useState(0);
+
+	useEffect(() => {
+		const arr = images.map((_, i) => i);
+		for (let i = arr.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[arr[i], arr[j]] = [arr[j]!, arr[i]!];
+		}
+		setOrder(arr);
+	}, [images]);
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-			setIndex((prevCounter) => (prevCounter + 1) % images.length);
+			setStep((prev) => (prev + 1) % images.length);
 		}, 5000);
-	
-		return () => clearInterval(interval);
-	}, []);
 
-	useEffect(() => {
-		setIndex((prevCounter) => (prevCounter + 1) % images.length);
-	}, []);
+		return () => clearInterval(interval);
+	}, [images.length]);
+
+	const activeIndex = order[step] ?? 0;
 
 	return (<div className={css.show}>
-		{images.map((image, i) => (<img key={i} className={i === index  ? `${css.img} ${css.active}` : css.img} src={image.src} width={image.width} height={image.height} alt={i === 0 ? imageAlt : ""} />))}
+		{images.map((image, i) => (<img key={i} className={i === activeIndex  ? `${css.img} ${css.active}` : css.img} src={image.src} width={image.width} height={image.height} alt={i === 0 ? imageAlt : ""} />))}
 	</div>);
 };
